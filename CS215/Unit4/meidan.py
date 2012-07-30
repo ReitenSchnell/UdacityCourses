7ac2f38ff11e686e632bfad1d6b51085fc45e292
reiten_schnell <alina542.1@gmail.com>
2012-07-30 19:17:34 +0300

    added median

---
+++ CS215/Unit4/meidan.py
@@ -0,0 +1,26 @@
+def minimize_absolute(L):
+    ind = len(L)/2
+    while len(L):
+        ind, L = arrange_array(ind, L)
+    return ind
+
+
+def arrange_array(ind, A):
+    val = A[ind]
+    bigger =  [A[i] for i in range(len(A)) if A[i] > val and i != ind]
+    smaller = [A[i] for i in range(len(A)) if A[i] <= val and i != ind]
+    print smaller, val, bigger
+    after_sort_position = len(smaller)
+    if after_sort_position == ind:
+            return A[ind], []
+    if after_sort_position > ind:
+        return ind, smaller
+    else:
+        return ind - len(smaller) - 1, bigger
+
+
+l = [6,5,4,5]
+result = minimize_absolute(l)
+print result
+print sorted(l)
+
