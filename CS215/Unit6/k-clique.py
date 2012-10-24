818f21b28a1c5e2b4de173d4cf94321a7a46dab8
Alina <alina542.1@gmail.com>
2012-10-24 11:05:58 +0300

    ps6-2

--- CS215/Unit6/k-clique.py
+++ CS215/Unit6/k-clique.py
@@ -2,11 +2,12 @@
 # Show how a k-clique can be found using a solution to the k-clique decision 
 # problem.  Write a Python function that takes a graph G and a number k 
 # as input, and returns a list of k nodes from G that are all connected 
-# in the graph.  Your function should make use of “k_clique_decision(G, k)”, 
-# which takes a graph G and a number k and answers whether G contains a k-clique.  
+# which takes a graph G and a number k and answers whether G contains a k-clique.
 # We will also provide the standard routines for adding and removing edges from a graph.
 
 # Returns a list of all the subsets of a list of size k
+from unittest import TestCase
+
 def k_subsets(lst, k):
     if len(lst) < k:
         return []
@@ -61,4 +62,50 @@ def break_link(G, node1, node2):
 def k_clique(G, k):
     if not k_clique_decision(G, k):
         return False
-        # your code here
+    for node1 in G:
+        for node2 in G:
+            if node1 < node2 and node2 in G[node1]:
+                G = break_link(G, node1, node2)
+                clique = k_clique(G, k)
+                if not clique:
+                    G = make_link(G, node1, node2)
+    return sorted([k for k in G.keys() if (len(G[k]) and k>1) or (k == 1)])
+
+class CliqueTests(TestCase):
+    def test_k_clique_decision_Should_be_true(self):
+        n1, n2, n3, n4, n5 = '1', '2', '3', '4', '5'
+        H = {}
+        make_link(H, n1, n2)
+        make_link(H, n1, n3)
+        make_link(H, n2, n3)
+        make_link(H, n2, n4)
+        make_link(H, n3, n5)
+        result = k_clique_decision(H, 3)
+        self.assertEquals(result, True)
+
+    def test_k_clique_decision_Should_be_false(self):
+        n1, n2, n3, n4, n5 = '1', '2', '3', '4', '5'
+        H = {}
+        make_link(H, n1, n2)
+        make_link(H, n1, n3)
+        make_link(H, n2, n5)
+        make_link(H, n2, n4)
+        make_link(H, n3, n5)
+        result = k_clique_decision(H, 3)
+        self.assertEquals(result, False)
+
+    def test_k_clique_should_return_clique_nodes(self):
+        n1, n2, n3, n4, n5 = '1', '2', '3', '4', '5'
+        H = {}
+        make_link(H, n1, n2)
+        make_link(H, n1, n3)
+        make_link(H, n2, n3)
+        make_link(H, n2, n4)
+        make_link(H, n3, n5)
+        result = k_clique(H, 3)
+        self.assertEquals(result, [n1, n2, n3])
+
+    def test_single_node_should_return_the_node(self):
+        G = {1:{}}
+        result = k_clique(G, 1)
+        self.assertEqual(result, [1])
